5133d8a7564ea485a2498dd41d5b5f0255a24027
vfairyo <1002076545@qq.com>
2025-02-06 14:53:30 +0800

    dis completed

--- Discussion/dis01.py
+++ Discussion/dis01.py
@@ -1,4 +1,5 @@
 import doctest
+from doctest import run_docstring_examples
 import math
 def race(x, y):
     """The tortoise always walks x feet per minute, while the hare repeatedly
@@ -58,5 +59,68 @@ def is_prime(n):
     for i in range(3, max_divisor, 2): # 跳过偶数
         if n % i == 0:
             return False
+    return True
+doctest.testmod(verbose=True)
+
+
+def has_digit(n, k):
+    """Returns whether k is a digit in n.
+
+    >>> has_digit(10, 1)
+    True
+    >>> has_digit(12, 7)
+    False
+    """
+    assert k >= 0 and k < 10
+    "*** YOUR CODE HERE ***"
+    if n == 0:
+        return k == 0
+    while n != 0:
+        digit = n % 10
+        n = n // 10
+        if digit == k: return True
+    return False
+# run_docstring_examples(has_digit, globals(),True)
+
+def unique_digits(n):
+    """Return the number of unique digits in positive integer n.
+
+    >>> unique_digits(8675309) # All are unique
+    7
+    >>> unique_digits(13173131) # 1, 3, and 7
+    3
+    >>> unique_digits(101) # 0 and 1
+    2
+    """
+    "*** YOUR CODE HERE ***"
+    cnt = 0
+    for num in range(0,10):
+        if has_digit(n, num):
+            cnt += 1
+    return cnt
+# run_docstring_examples(unique_digits, globals(),True)
+# doctest.testmod(verbose=True)
+def repeating(t, n):
+    """Return whether t digits repeat to form positive integer n.
+
+    >>> repeating(1, 6161)
+    False
+    >>> repeating(2, 6161)  # repeats 61 (2 digits)
+    True
+    >>> repeating(3, 6161)
+    False
+    >>> repeating(4, 6161)  # repeats 6161 (4 digits)
+    True
+    >>> repeating(5, 6161)  # there are only 4 digits
+    False
+    """
+    if pow(10, t-1) > n:  # make sure n has at least t digits
+        return False
+    end = n % pow(10, t)
+    rest = n
+    while rest:
+        if rest % pow(10, t) != end:
+           return False
+        rest = rest // pow (10, t)
     return True
 doctest.testmod(verbose=True)
